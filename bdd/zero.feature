54c0fc63f9a7f9370c6ba9628a71c686f2939257
LukasBola <lukasz.bola@icloud.com>
2019-02-02 00:03:58 +0100

    Test from QA Has Power workshop using other way of giving arguments - convert quotation marks

--- bdd/zero.feature
+++ bdd/zero.feature
@@ -5,10 +5,10 @@
 Feature: Test of car dealer site
 
 	Scenario: log in 
-		Given the page http://salon.rpgit.pl/ is loaded	
-		When I type lukasz.bola in id_username input
-		And I type Welcome1 in id_password input
-		And I click id_login_btn button
+		Given the page "http://salon.rpgit.pl/" is loaded	
+		When I type "lukasz.bola" in "id_username" input
+		And I type "Welcome1" in "id_password" input
+		And I click "id_login_btn" button
 		# Then the home page is diplayed
 
 
--- steps/steps.py
+++ steps/steps.py
@@ -4,18 +4,18 @@
 from behave import *
 import time
 
-@given("the page {url} is loaded")
+@given('the page "{url}" is loaded')
 def step_impl(context, url): 
 	context.browser.get(url)
 	time.sleep(1)
 	
-@when("I type {text} in {input_id_field} input")
+@when('I type "{text}" in "{input_id_field}" input')
 def step_impl(context, text, input_id_field):
 	input_field = context.browser.find_element_by_xpath('//input [@id = "{}"]'.format(input_id_field))
 	input_field.send_keys(text)
 	time.sleep(1)
 
-@when("I click {button_id} button")
+@when('I click "{button_id}" button')
 def step_impl(context, button_id):
 	button = context.browser.find_element_by_xpath('//button [@id = "{}"]'.format(button_id))
 	button.click()
